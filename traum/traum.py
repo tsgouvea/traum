1e17e85bf42da832e1de053c75856f7dd887822f
Thiago Gouvêa <tsgouvea@gmail.com>
2018-04-20 17:27:00 -0400

    Plots rasters and PETHs

--- traum/traum.py
+++ traum/traum.py
@@ -9,9 +9,10 @@ class traum:
 
     def __init__(self,dataBhv):
         self.bhv = dataBhv.parsedData
+        self.bhv['tsState0'] = self.bhv['tsState0']-self.bhv['tsState0'][0]
         self.neur = pd.DataFrame({'spikes': [], 'dataset':[], 'cluster':[]})#, 'waveform_mean':[], 'waveform_std':[], 'nTrode':[], 'cluster':[]})
 
-    def readDio(self,pathDio):
+    def readDio(self,pathDio,fs=30000):
         listDio = os.listdir(pathDio)
         listDio.sort()
         df_singleChanges = pd.DataFrame({'channel': [], 'state': [], 'time': []})
@@ -40,6 +41,7 @@ class traum:
             state.append(int(stateBin[-1], 2))
 
         self.dio = pd.DataFrame({'time':time, 'state':state, 'stateBin':stateBin})
+        self.dio['time'] = self.dio['time']/fs
         self.sync()
 
     def sync(self):
@@ -63,10 +65,12 @@ class traum:
             #print('Off sync - len(tsDio) > len(tsBhv)')
             i, delta, rhos = trim(tsDio,tsBhv)
             self.dio = self.dio[(self.dio['time'] < tsDio.iloc[i+len(tsBhv)]) & (self.dio['time'] >= tsDio.iloc[i])]
+        self.aligEvent = list(self.bhv.columns[[n.startswith('ts') for n in self.bhv.columns]])
+        self.aligEvent.remove('tsState0')
         #self.tsDio = self.dio['time'][self.dio['state']==0]
         #self.tsBhv = self.bhv['tsState0']-self.bhv['tsState0'][0]
 
-    def readNeur(self,pathNeur,prefix='ms3',filename='firings.curated.mda'):
+    def readNeur(self,pathNeur,prefix='ms3',filename='firings.curated.mda',fs=30000):
 
         listNt = np.array(os.listdir(pathNeur))
         listNt = listNt[[n.startswith(prefix) for n in listNt]]
@@ -81,7 +85,20 @@ class traum:
                 ndx = mdaCurated[2,:] == setClust[i]
                 df_spikes[i] = mdaCurated[1,ndx]
                 df_cluster[i] = int(setClust[i])
-            self.neur = self.neur.append(pd.DataFrame({'spikes': df_spikes, 'cluster':df_cluster, 'dataset': nt}),ignore_index=True)
+            self.neur = self.neur.append(pd.DataFrame({'spikes': np.array(df_spikes)/fs, 'cluster':df_cluster, 'dataset': nt}),ignore_index=True)
 
+    def raspeth(self,alignment,iUnit,trialMask,panes,window=(-1,2)):
+        listAlign = np.array(self.dio['time'][self.dio['state']==0][trialMask]) + np.array(self.bhv[alignment][trialMask])
+        spikes = [[]]*len(listAlign)
 
+        for iTrial in range(len(listAlign)):
+            temp = self.neur['spikes'][iUnit] - listAlign[iTrial]
+            spikes[iTrial] = temp[(temp>=window[0]) & (temp<=window[1])]
+
+        ha_raster, ha_peth = panes
+        ha_raster.eventplot(spikes)
+
+        counts, edges = np.histogram([item for sublist in spikes for item in sublist], bins='auto', density=False)
+        ha_peth.plot(edges[:-1]+(edges[1]-edges[0])/2,counts)
+        return counts,edges
             #self.neur = self.neur[i:-delta]
