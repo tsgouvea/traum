f15249d300b66599366383ab5a746127b36e1fdd
Thiago Gouvêa <tsgouvea@gmail.com>
2018-04-23 10:52:48 -0400

    Producing rasters & PETHs. Now expanding on behavioral annotation.

--- traum/traum.py
+++ traum/traum.py
@@ -8,8 +8,8 @@ from mdaio import readmda
 class traum:
 
     def __init__(self,dataBhv):
-        self.bhv = dataBhv.parsedData
-        self.bhv['tsState0'] = self.bhv['tsState0']-self.bhv['tsState0'][0]
+        self.bhv = dataBhv
+        self.bhv.parsedData['tsState0'] = self.bhv.parsedData['tsState0']-self.bhv.parsedData['tsState0'][0]
         self.neur = pd.DataFrame({'spikes': [], 'dataset':[], 'cluster':[]})#, 'waveform_mean':[], 'waveform_std':[], 'nTrode':[], 'cluster':[]})
 
     def readDio(self,pathDio,fs=30000):
@@ -22,11 +22,10 @@ class traum:
             df_temp = pd.DataFrame(x['data'])
             df_temp['channel'] = iDio
             df_singleChanges = df_singleChanges.append(df_temp)
-
         timeSet = sorted(set(df_singleChanges['time']))
         t0 = timeSet[0]
         timeSet.remove(t0)
-        time = [t0-t0]
+        time = [t0]
         stateBin = ['0'*len(set(df_singleChanges['channel']))]
         state = [int(stateBin[-1], 2)]
 
@@ -36,13 +35,29 @@ class traum:
             listStates = df_t['state']
             for c in df_t['channel']:
                 sbin[int(c)] = str(int(listStates[c==df_t['channel']]))
-            time.append(t-t0)
+            time.append(t)
             stateBin.append(''.join(sbin)[::-1])
             state.append(int(stateBin[-1], 2))
 
-        self.dio = pd.DataFrame({'time':time, 'state':state, 'stateBin':stateBin})
-        self.dio['time'] = self.dio['time']/fs
-        self.sync()
+        self.dio = pd.DataFrame({'time':np.array(time)/fs, 'state':state, 'stateBin':stateBin})
+        self.dio['time'] = self.dio['time']
+
+    def readNeur(self,pathNeur,prefix='ms3',filename='firings.curated.mda',fs=30000):
+        listNt = np.array(os.listdir(pathNeur))
+        listNt = listNt[[n.startswith(prefix) for n in listNt]]
+        assert(len(listNt)>0)
+        for nt in listNt:
+            mdaCurated = readmda(os.path.join(pathNeur,nt,filename))
+            setClust = list(set(mdaCurated[2,:]))
+            setClust.sort()
+            df_spikes = [[]]*len(setClust)
+            df_cluster = np.empty(len(setClust),np.int)
+            for i in range(len(setClust)):
+                ndx = mdaCurated[2,:] == setClust[i]
+                df_spikes[i] = mdaCurated[1,ndx]
+                df_cluster[i] = int(setClust[i])
+            self.neur = self.neur.append(pd.DataFrame({'spikes': np.array(df_spikes)/fs, 'cluster':df_cluster, 'dataset': nt}),ignore_index=True)
+        self.rawNeur = mdaCurated
 
     def sync(self):
 
@@ -55,40 +70,19 @@ class traum:
             return(np.argmax(rhos), delta, rhos)
 
         tsDio = self.dio['time'][self.dio['state']==0]
-        tsBhv = self.bhv['tsState0']-self.bhv['tsState0'][0]
+        tsBhv = self.bhv.parsedData['tsState0']-self.bhv.parsedData['tsState0'][0]
 
         if len(tsBhv) > len(tsDio):
-            #print('Off sync - len(tsBhv) > len(tsDio)')
             i, delta, rhos = trim(tsBhv,tsDio)
-            self.bhv = self.bhv[i:len(tsDio)+i]
+            self.bhv.parsedData = self.bhv.parsedData[i:len(tsDio)+i]
         elif len(tsDio) > len(tsBhv):
-            #print('Off sync - len(tsDio) > len(tsBhv)')
             i, delta, rhos = trim(tsDio,tsBhv)
             self.dio = self.dio[(self.dio['time'] < tsDio.iloc[i+len(tsBhv)]) & (self.dio['time'] >= tsDio.iloc[i])]
-        self.aligEvent = list(self.bhv.columns[[n.startswith('ts') for n in self.bhv.columns]])
+        self.aligEvent = list(self.bhv.parsedData.columns[[n.startswith('ts') for n in self.bhv.parsedData.columns]])
         self.aligEvent.remove('tsState0')
-        #self.tsDio = self.dio['time'][self.dio['state']==0]
-        #self.tsBhv = self.bhv['tsState0']-self.bhv['tsState0'][0]
-
-    def readNeur(self,pathNeur,prefix='ms3',filename='firings.curated.mda',fs=30000):
-
-        listNt = np.array(os.listdir(pathNeur))
-        listNt = listNt[[n.startswith(prefix) for n in listNt]]
-        assert(len(listNt)>0)
-        for nt in listNt:
-            mdaCurated = readmda(os.path.join(pathNeur,nt,filename))
-            setClust = list(set(mdaCurated[2,:]))
-            setClust.sort()
-            df_spikes = [[]]*len(setClust)
-            df_cluster = np.empty(len(setClust),int)
-            for i in range(len(setClust)):
-                ndx = mdaCurated[2,:] == setClust[i]
-                df_spikes[i] = mdaCurated[1,ndx]
-                df_cluster[i] = int(setClust[i])
-            self.neur = self.neur.append(pd.DataFrame({'spikes': np.array(df_spikes)/fs, 'cluster':df_cluster, 'dataset': nt}),ignore_index=True)
 
     def raspeth(self,alignment,iUnit,trialMask,panes,window=(-1,2)):
-        listAlign = np.array(self.dio['time'][self.dio['state']==0][trialMask]) + np.array(self.bhv[alignment][trialMask])
+        listAlign = np.array(self.dio['time'][self.dio['state']==0][trialMask]) + np.array(self.bhv.parsedData[alignment][trialMask])
         spikes = [[]]*len(listAlign)
 
         for iTrial in range(len(listAlign)):
@@ -99,6 +93,9 @@ class traum:
         ha_raster.eventplot(spikes)
 
         counts, edges = np.histogram([item for sublist in spikes for item in sublist], bins='auto', density=False)
-        ha_peth.plot(edges[:-1]+(edges[1]-edges[0])/2,counts)
-        return counts,edges
-            #self.neur = self.neur[i:-delta]
+        ha_peth.plot(edges[:-1]+(edges[1]-edges[0])/2,counts/(edges[1]-edges[0])/len(listAlign))
+        ha_raster.set_ylabel('Trial #')
+        ha_raster.set_title(alignment)
+        ha_peth.set_ylabel('Firing rate')
+        ha_peth.set_xlabel('Time (s)')
+        return ha_raster, ha_peth
